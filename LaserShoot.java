b0c337bff9baac90dc154c53a40010c0982f11e0
yiduoke <yiduoke822@jhs185.org>
2017-01-09 11:12:43 +0800

    Add files via upload
    MOVING AND ROTATING OF ONE MIRROR WITH KEYBOARD

--- LaserShoot.java
+++ LaserShoot.java
@@ -1,42 +1,95 @@
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-@SuppressWarnings("serial")
-public class LaserShoot extends JPanel{
+public class LaserShoot extends JPanel implements ActionListener, KeyListener{
 	
-	Mirror one=new Mirror(this, 400,300,8,60);
-	Mirror two=new Mirror(this,600,400,8,60);
-
-	public void move(){
-		one.move();
-		two.move();
+	Timer tm = new Timer(5, this);
+	double x=0, velX=0, y=0, velY=0;
+	double theta=0, theta1=0;
+	public LaserShoot(){
+		tm.start();
+		addKeyListener(this);
+		setFocusable(true);//enables keylistener
+		setFocusTraversalKeysEnabled(false);
 	}
-	
-	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
-		Graphics2D g2d = (Graphics2D) g;
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		g.setColor(Color.RED);
+		Graphics2D g2d=(Graphics2D)g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
-		one.paint(g2d);
-		two.paint(g2d);
+		g2d.rotate(theta,x+50,y+3);
+		g2d.fillRect((int)x,(int)y,100,6);
 	}
 	
-	public static void main(String[] args) throws InterruptedException {
-		JFrame frame = new JFrame("Laser Shoot");
-		LaserShoot laserShoot= new LaserShoot();
-		frame.add(laserShoot);
-		frame.setSize(1000, 600);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		while (true) {
-			laserShoot.move();
-			laserShoot.repaint();
-			Thread.sleep(10);
+	public void actionPerformed(ActionEvent e){
+		if (x<0){
+			velX=0;
+			x=0;
+		}
+		if (x>640){
+			velX=0;
+			x=640;
+		}
+		if (y<0){
+			velY=0;
+			y=0;
+		}
+		if (y>640){
+			velY=0;
+			y=640;
+		}
+		x+=velX;
+		y+=velY;
+		repaint();//calls paintcomponent
+	}
+	
+	public void keyPressed(KeyEvent e){
+		int c=e.getKeyCode();
+		if (c== KeyEvent.VK_LEFT){
+			velY=0;
+			velX=-0.5;
+		}
+		if (c== KeyEvent.VK_RIGHT){
+			velY=0;
+			velX=0.5;
+		}
+		if (c==KeyEvent.VK_UP){
+			velY=-0.5;
+			velX=0;
+		}
+		if (c== KeyEvent.VK_DOWN){
+			velX=0;
+			velY=0.5;
+		}
+		if (c== KeyEvent.VK_R){
+			theta+=0.05;
 		}
 	}
+	
+	public void keyTyped(KeyEvent e){
+	}
+	public void keyReleased(KeyEvent e){
+		velX=0;
+		velY=0;
+	}
+	
+public static void main (String[] args){
+	LaserShoot marg = new LaserShoot();
+	JFrame frame= new JFrame("HIIII");
+	frame.setVisible(true);
+	frame.setSize(700,700);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.add(marg);
+}
 }
