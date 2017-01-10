80f3dc90e51f2638ae5e656edd20a46724da1ec7
Winston Chen <Winston@Winstons-iMac.home>
2017-01-09 21:32:34 -0500

    worked on laser

--- Laser.java
+++ Laser.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
+import java.math.*;
 
 import javax.swing.*;
 
@@ -11,6 +12,7 @@ public class Laser extends JComponent{
 	final int y1;
 	final int x2;
 	final int y2;   
+	final double theta;
 	final Color color;
 
 	public Line(int x1, int y1, int x2, int y2, Color color) {
@@ -18,6 +20,7 @@ public class Laser extends JComponent{
 	    this.y1 = y1;
 	    this.x2 = x2;
 	    this.y2 = y2;
+	    this.theta = atan((y2-y1)/(x2-x1));
 	    this.color = color;
 	}               
     }
@@ -25,11 +28,11 @@ public class Laser extends JComponent{
     private final LinkedList<Line> lines = new LinkedList<Line>();
 
     public void addLine(int x1, int x2, int x3, int x4) {
-	addLine(x1, x2, x3, x4, Color.black);
+	addLine(x1, x2, x3, x4, Color.red);
     }
 
     public void addLine(int x1, int x2, int x3, int x4, Color color) {
-	lines.add(new Line(x1,x2,x3,x4, color));        
+	lines.add(new Line(x1, x2, x3, x4, color));        
 	repaint();
     }
 
@@ -45,5 +48,43 @@ public class Laser extends JComponent{
 	    g.setColor(line.color);
 	    g.drawLine(line.x1, line.y1, line.x2, line.y2);
 	}
+    }
+
+    public static void main (String[] args){
+	LaserShoot marg = new LaserShoot();
+	JFrame frame= new JFrame("HIIII");
+	frame.setVisible(true);
+	frame.setSize(700,700);
+	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	frame.add(marg);
+	final Laser comp = new Laser();
+	comp.setPreferredSize(new Dimension(700, 700));
+	frame.getContentPane().add(comp, BorderLayout.CENTER);
+	JPanel buttonsPanel = new JPanel();
+	JButton laserButton = new JButton("Fire Laser");
+	JButton clearButton = new JButton("Clear");
+	buttonsPanel.add(laserButton);
+	buttonsPanel.add(clearButton);
+	frame.getContentPane().add(buttonsPanel, BorderLayout.SOUTH);
+	laserButton.addActionListener(new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+		    int x1 = 0;
+		    int x2 = 700;
+		    int y1 = 350;
+		    int y2 = 350;
+		    comp.addLine(x1, y1, x2, y2, Color.red);
+		}
+	    });
+	clearButton.addActionListener(new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+		    comp.clearLines();
+		}
+	    });
+	frame.pack();
+	frame.setVisible(true);
     }
 }
