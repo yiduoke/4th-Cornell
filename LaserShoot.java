31f8bfe9ae8883d2ed4b488160537f5ead3e9149
yiduoke <yiduoke822@jhs185.org>
2017-01-10 03:02:00 -0500

    Add files via upload
    Two mirrors, a mid line, a source dot, a target dot

--- LaserShoot.java
+++ LaserShoot.java
@@ -16,13 +16,15 @@ public class LaserShoot extends JPanel implements ActionListener, KeyListener{
 	
 	Timer tm = new Timer(5, this);
 	Mirror margaret;
+	Mirror winston;
     
 	public LaserShoot(){
 		tm.start();
 		addKeyListener(this);
 		setFocusable(true);//enables keylistener
 		setFocusTraversalKeysEnabled(false);
-		margaret=new Mirror(100,100,250,100);
+		margaret=new Mirror(100,300,250,300);
+		winston=new Mirror(600,500,750,500);
 	}
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
@@ -30,16 +32,33 @@ public class LaserShoot extends JPanel implements ActionListener, KeyListener{
 		Graphics2D g2d=(Graphics2D)g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
+		//mirrors
 		g2d.setStroke(new BasicStroke(5));
 		g2d.drawLine(margaret.x1,margaret.y1,margaret.x2,margaret.y2);
+
+		g2d.drawLine(winston.x1,winston.y1,winston.x2,winston.y2);
+		
+		//midline
+		g2d.setStroke(new BasicStroke(1));
+		g.setColor(Color.BLACK);
+		g2d.drawLine(0,350,1300,350);
+		
+		//laser dot
+		g.setColor(Color.RED);
+		g2d.fillOval(1270, 343, 15, 15);
+		
+		g.setColor(Color.GREEN);
+		g2d.fillOval(0, 343, 15, 15);
+		
 		}
 	
 	public void actionPerformed(ActionEvent e){
-		margaret.move(margaret.thetaChange);
 		margaret.x1+=margaret.xa;
 		margaret.x2+=margaret.xa;
 		margaret.y1+=margaret.ya;
 		margaret.y2+=margaret.ya;
+		margaret.move(margaret.thetaChange);
+		winston.move(winston.thetaChange);
 		repaint();//calls paintcomponent
 	}
 	
@@ -47,28 +66,33 @@ public class LaserShoot extends JPanel implements ActionListener, KeyListener{
 		int c=e.getKeyCode();
 		if (c== KeyEvent.VK_LEFT){
 			margaret.ya=0;
-			margaret.xa=-0.5;
+			margaret.xa=-1;
 			margaret.thetaChange=0;
 		}
 		if (c== KeyEvent.VK_RIGHT){
 			margaret.ya=0;
-			margaret.xa=0.5;
+			margaret.xa=1;
 			margaret.thetaChange=0;
 		}
 		if (c==KeyEvent.VK_UP){
-			margaret.ya=-0.5;
+			margaret.ya=-1;
 			margaret.xa=0;
 			margaret.thetaChange=0;
 		}
 		if (c== KeyEvent.VK_DOWN){
 			margaret.xa=0;
-			margaret.ya=0.5;
+			margaret.ya=1;
 			margaret.thetaChange=0;
 		}
-		if (c== KeyEvent.VK_R){
+		if (c== KeyEvent.VK_M){
 			margaret.thetaChange=0.005;
 			margaret.xa=0;
 			margaret.ya=0;
+		}
+		if (c== KeyEvent.VK_W){
+			winston.thetaChange=0.005;
+			winston.xa=0;
+			winston.ya=0;
 		}
 	}
 	
@@ -76,13 +100,14 @@ public class LaserShoot extends JPanel implements ActionListener, KeyListener{
 	}
 	public void keyReleased(KeyEvent e){
 		margaret.thetaChange=0;
+		winston.thetaChange=0;
 		margaret.xa=0;
 		margaret.ya=0;
 	}
 	
 public static void main (String[] args){
 	LaserShoot marg = new LaserShoot();
-	JFrame frame= new JFrame("HIIII");
+	JFrame frame= new JFrame("Laser Shoot");
 	frame.setVisible(true);
 	frame.setSize(1300,700);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- Mirror.java
+++ Mirror.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 
