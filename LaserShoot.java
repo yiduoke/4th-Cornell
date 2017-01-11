e4152a14cd3355ea45da549518d3374be79a6f93
yiduoke <yiduoke822@jhs185.org>
2017-01-11 16:26:47 -0500

    Add files via upload

--- LaserShoot.java
+++ LaserShoot.java
@@ -14,104 +14,111 @@ import javax.swing.Timer;
 
 public class LaserShoot extends JPanel implements ActionListener, KeyListener{
 	
-	Timer tm = new Timer(5, this);
-	Mirror margaret;
-	Mirror winston;
+    Timer tm = new Timer(5, this);
+    Mirror margaret;
+    Mirror winston;
     
-	public LaserShoot(){
-		tm.start();
-		addKeyListener(this);
-		setFocusable(true);//enables keylistener
-		setFocusTraversalKeysEnabled(false);
-		margaret=new Mirror(100,300,250,300);
-		winston=new Mirror(600,500,750,500);
-	}
-	public void paintComponent(Graphics g){
-		super.paintComponent(g);
-		g.setColor(Color.BLUE);
-		Graphics2D g2d=(Graphics2D)g;
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-				RenderingHints.VALUE_ANTIALIAS_ON);
-		//mirrors
-		g2d.setStroke(new BasicStroke(5));
-		g2d.drawLine(margaret.x1,margaret.y1,margaret.x2,margaret.y2);
+    public LaserShoot(){
+	tm.start();
+	addKeyListener(this);
+	setFocusable(true);//enables keylistener
+	setFocusTraversalKeysEnabled(false);
+	margaret=new Mirror(100,300,250,300);
+	winston=new Mirror(600,500,750,500);
+    }
+    public void paintComponent(Graphics g){
+	super.paintComponent(g);
+	g.setColor(Color.BLUE);
+	Graphics2D g2d=(Graphics2D)g;
+	g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+			     RenderingHints.VALUE_ANTIALIAS_ON);
+	//mirrors
+	g2d.setStroke(new BasicStroke(5));
+	g2d.drawLine(margaret.x1,margaret.y1,margaret.x2,margaret.y2);
 
-		g2d.drawLine(winston.x1,winston.y1,winston.x2,winston.y2);
+	g2d.drawLine(winston.x1,winston.y1,winston.x2,winston.y2);
 		
-		//midline
-		g2d.setStroke(new BasicStroke(1));
-		g.setColor(Color.BLACK);
-		g2d.drawLine(0,350,1300,350);
+	//midline
+	g2d.setStroke(new BasicStroke(1));
+	g.setColor(Color.BLACK);
+	g2d.drawLine(0,350,1300,350);
 		
-		//laser
-		g.setColor(Color.GREEN);
-		g2d.fillOval(1280, 343, 15, 15);
+	//laser
+	g.setColor(Color.GREEN);
+	g2d.fillOval(1280, 343, 15, 15);
 		
-		//target
-		g.setColor(Color.RED);
-		g2d.fillOval(0, 343, 15, 15);
+	//target
+	g.setColor(Color.RED);
+	g2d.fillOval(0, 343, 15, 15);
+
+	//dots on end of mirrors for reference in angle calculation
+	g.setColor(Color.ORANGE);
+	g2d.fillOval(margaret.x2,margaret.y2,10,10);
+	g2d.fillOval(winston.x2,winston.y2,10,10);
 		
-		}
+    }
+
+    public void actionPerformed(ActionEvent e){
+	margaret.rotate(margaret.thetaChange);
+	winston.rotate(winston.thetaChange);
+	margaret.translate(margaret.xa,margaret.ya);
+	winston.translate(winston.xa,winston.ya);
+	repaint();//calls paintcomponent
+    }
 	
-	public void actionPerformed(ActionEvent e){
-		margaret.x1+=margaret.xa;
-		margaret.x2+=margaret.xa;
-		margaret.y1+=margaret.ya;
-		margaret.y2+=margaret.ya;
-		margaret.move(margaret.thetaChange);
-		winston.move(winston.thetaChange);
-		repaint();//calls paintcomponent
+    public void keyPressed(KeyEvent e){
+	int c=e.getKeyCode();
+	if (c== KeyEvent.VK_LEFT){
+	    System.out.println("Left");
+	    margaret.ya=0;
+	    margaret.xa=-1;
+	    margaret.thetaChange=0;
+	    //System.out.println(margaret.ya);
+	    //System.out.println(margaret.xa);
 	}
+	if (c== KeyEvent.VK_RIGHT){
+	    margaret.ya=0;
+	    margaret.xa=1;
+	    //margaret.thetaChange=0;
+	}
+	if (c==KeyEvent.VK_UP){
+	    margaret.ya=-1;
+	    margaret.xa=0;
+	    //margaret.thetaChange=0;
+	}
+	if (c== KeyEvent.VK_DOWN){
+	    margaret.xa=0;
+	    margaret.ya=1;
+	    //Xmargaret.thetaChange=0;
+	}
+	if (c== KeyEvent.VK_M){
+	    margaret.thetaChange=0.005;
+	    //margaret.xa=0;
+	    //margaret.ya=0;
+	}
+	if (c== KeyEvent.VK_W){
+	    winston.thetaChange=0.005;
+	    winston.xa=0;
+	    winston.ya=0;
+	}
+    }
 	
-	public void keyPressed(KeyEvent e){
-		int c=e.getKeyCode();
-		if (c== KeyEvent.VK_LEFT){
-			margaret.ya=0;
-			margaret.xa=-1;
-			margaret.thetaChange=0;
-		}
-		if (c== KeyEvent.VK_RIGHT){
-			margaret.ya=0;
-			margaret.xa=1;
-			margaret.thetaChange=0;
-		}
-		if (c==KeyEvent.VK_UP){
-			margaret.ya=-1;
-			margaret.xa=0;
-			margaret.thetaChange=0;
-		}
-		if (c== KeyEvent.VK_DOWN){
-			margaret.xa=0;
-			margaret.ya=1;
-			margaret.thetaChange=0;
-		}
-		if (c== KeyEvent.VK_M){
-			margaret.thetaChange=0.005;
-			margaret.xa=0;
-			margaret.ya=0;
-		}
-		if (c== KeyEvent.VK_W){
-			winston.thetaChange=0.005;
-			winston.xa=0;
-			winston.ya=0;
-		}
-	}
+    public void keyTyped(KeyEvent e){
+    }
+
+    public void keyReleased(KeyEvent e){
+	margaret.thetaChange=0;
+	winston.thetaChange=0;
+	margaret.xa=0;
+	margaret.ya=0;
+    }
 	
-	public void keyTyped(KeyEvent e){
-	}
-	public void keyReleased(KeyEvent e){
-		margaret.thetaChange=0;
-		winston.thetaChange=0;
-		margaret.xa=0;
-		margaret.ya=0;
-	}
-	
-public static void main (String[] args){
+    public static void main (String[] args){
 	LaserShoot marg = new LaserShoot();
 	JFrame frame= new JFrame("Laser Shoot");
 	frame.setVisible(true);
 	frame.setSize(1300,700);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	frame.add(marg);
-}
+    }
 }
--- Mirror.java
+++ Mirror.java
@@ -4,13 +4,13 @@ import java.awt.Rectangle;
 
 
 public class Mirror {
-    double midX, midY;
-    int x1, y1, x2, y2;
-    double xa;
-    double ya;
-    double theta = 0;
+    double midX,midY;
+    int x1,y1,x2,y2;
+    int xa;
+    int ya;
+    double theta=0;
     double thetaChange;
-    double length;
+    LaserShoot laserShoot;
 
     public Mirror(int a,int b,int c,int d){
     	x1=a;
@@ -19,17 +19,24 @@ public class Mirror {
     	y2=d;
     	midX=(x1+x2)/2;
     	midY=(y1+y2)/2;
-	length = Math.sqrt((y2-y1)*(y2-y1)+(x2-x1)*(x2-x1));
     }
 
-    public void move(double t) {
+    public void rotate(double t) {
 	theta+=t;
 	x1=(int) (midX-Math.cos(theta)*75);
 	x2=(int) (midX+Math.cos(theta)*75);
 	y1=(int) (midY+Math.sin(theta)*75);
 	y2=(int) (midY-Math.sin(theta)*75);
-    }
+	}
 
+    public void translate(int xVel, int yVel){
+        x1+=xVel;
+	x2+=xVel;
+	y1+=yVel;
+	y2+=yVel;
+	midX+=xVel;
+	midY+=yVel;
+    }
 
     public void paint(Graphics2D g) {
 	g.drawLine((int)x1, (int)y1, (int)x2, (int)y2);
