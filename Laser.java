b0fb76f335c895610d3498321eea2ffdcb1b7a23
Winston <winstonchen88899@gmail.com>
2017-01-12 12:31:39 -0500

    comments and minor corrections

--- Laser.java
+++ Laser.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 
 public class Laser{
 
-    int x, y;//should we change these to doubles
+    int x, y; //change these to doubles?
     double theta;
     boolean on;
 
@@ -13,23 +13,27 @@ public class Laser{
 	this.x = x;
 	this.y = y;
 	theta = 0;
-	on = false;//should not be on at first wINSTON
+	on = false;
     }
-
-    private double distance(int x1, int y1, int x2, int y2){
+    
+    private double distance(int x1, int y1, int x2, int y2){ //distance formula
 	return Math.sqrt((y2-y1)*(y2-y1)+(x2-x1)*(x2-x1));
     }
-
+    
     public boolean intersect(Mirror m){ //abuses triangle inequality theorem
 	double d1 = distance(x, y, m.x1, m.y1);
 	double d2 = distance(x, y, m.x2, m.y2);
 	return (d1 + d2 >= distance(m.x1,m.y1,m.x2,m.y2)*0.9999 || d1 + d2 <= distance(m.x1,m.y1,m.x2,m.y2)*1.0001);
     }
 
-    public void reflect(Mirror m){
-    	if (m.theta==0 || m.theta==Math.PI){on=false;}
-    	else {theta = Math.toRadians(360-Math.toDegrees(theta)+2*Math.toDegrees(m.theta));}
+    public void reflect(Mirror m){ //angle calculations!
+    	if (m.theta==0 || m.theta==Math.PI){
+	    on=false;
 	}
+    	else {
+	    theta = Math.toRadians(360-Math.toDegrees(theta)+2*Math.toDegrees(m.theta));
+	}
+    }
     
     public void propagate(){
     	x+=0.01;
