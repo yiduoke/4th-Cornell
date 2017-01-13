f56edf54067c0ed7de1d55dfc000077c3a410848
yiduoke <yiduoke822@jhs185.org>
2017-01-13 16:02:36 -0500

    Add files via upload

--- LaserShoot.java
+++ LaserShoot.java
@@ -17,19 +17,14 @@ public class LaserShoot extends JPanel implements ActionListener, KeyListener{
     Timer tm = new Timer(5, this);
     Mirror margaret;
     Mirror winston;
-    Laser L;
-    //Mirror[] wall;
     
     public LaserShoot(){
 	tm.start();
 	addKeyListener(this);
 	setFocusable(true);//enables keylistener
 	setFocusTraversalKeysEnabled(false);
-	margaret = new Mirror(100,300,250,300);
-	winston = new Mirror(600,500,750,500);
-	L = new Laser(0,300);
-	//Mirror[0]=margaret;
-	//Mirror[1]=winston;
+	margaret=new Mirror(100,300,250,300);
+	winston=new Mirror(600,500,750,500);
     }
     public void paintComponent(Graphics g){
 	super.paintComponent(g);
@@ -48,11 +43,11 @@ public class LaserShoot extends JPanel implements ActionListener, KeyListener{
 	g.setColor(Color.BLACK);
 	g2d.drawLine(0,350,1300,350);
 		
-	//target
+	//laser
 	g.setColor(Color.GREEN);
 	g2d.fillOval(1280, 343, 15, 15);
 		
-	//laser source
+	//target
 	g.setColor(Color.RED);
 	g2d.fillOval(0, 343, 15, 15);
 
@@ -60,11 +55,7 @@ public class LaserShoot extends JPanel implements ActionListener, KeyListener{
 	g.setColor(Color.ORANGE);
 	g2d.fillOval(margaret.x2,margaret.y2,10,10);
 	g2d.fillOval(winston.x2,winston.y2,10,10);
-
-	//laser
-	g.setColor(Color.RED);
-	g2d.fillOval(L.x,L.y,5,5);
-	//System.out.println(L.x);
+		
     }
 
     public void actionPerformed(ActionEvent e){
@@ -72,18 +63,12 @@ public class LaserShoot extends JPanel implements ActionListener, KeyListener{
 	winston.rotate(winston.thetaChange);
 	margaret.translate(margaret.xa,margaret.ya);
 	winston.translate(winston.xa,winston.ya);
-	if(L.on){
-	    L.propagate();
-	    if(L.intersect(margaret)){
-		L.reflect(margaret);
-	    }
-	}
 	repaint();//calls paintcomponent
     }
 	
     public void keyPressed(KeyEvent e){
-	int c = e.getKeyCode();
-	if (c == KeyEvent.VK_LEFT){
+	int c=e.getKeyCode();
+	if (c== KeyEvent.VK_LEFT){
 	    System.out.println("Left");
 	    margaret.ya=0;
 	    margaret.xa=-1;
@@ -91,35 +76,31 @@ public class LaserShoot extends JPanel implements ActionListener, KeyListener{
 	    //System.out.println(margaret.ya);
 	    //System.out.println(margaret.xa);
 	}
-	if (c == KeyEvent.VK_RIGHT){
+	if (c== KeyEvent.VK_RIGHT){
 	    margaret.ya=0;
 	    margaret.xa=1;
 	    //margaret.thetaChange=0;
 	}
-	if (c ==KeyEvent.VK_UP){
+	if (c==KeyEvent.VK_UP){
 	    margaret.ya=-1;
 	    margaret.xa=0;
 	    //margaret.thetaChange=0;
 	}
-	if (c == KeyEvent.VK_DOWN){
+	if (c== KeyEvent.VK_DOWN){
 	    margaret.xa=0;
 	    margaret.ya=1;
 	    //Xmargaret.thetaChange=0;
 	}
-	if (c == KeyEvent.VK_M){
+	if (c== KeyEvent.VK_M){
 	    margaret.thetaChange=0.005;
 	    //margaret.xa=0;
 	    //margaret.ya=0;
 	}
-	if (c == KeyEvent.VK_W){
+	if (c== KeyEvent.VK_W){
 	    winston.thetaChange=0.005;
 	    winston.xa=0;
 	    winston.ya=0;
 	}
-	if (c == KeyEvent.VK_L){
-	    L.on = true;
-	    //System.out.println(L.on);
-	} 
     }
 	
     public void keyTyped(KeyEvent e){
@@ -133,11 +114,11 @@ public class LaserShoot extends JPanel implements ActionListener, KeyListener{
     }
 	
     public static void main (String[] args){
-	LaserShoot LSS = new LaserShoot();
+	LaserShoot marg = new LaserShoot();
 	JFrame frame= new JFrame("Laser Shoot");
 	frame.setVisible(true);
 	frame.setSize(1300,700);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame.add(LSS);
+	frame.add(marg);
     }
 }
